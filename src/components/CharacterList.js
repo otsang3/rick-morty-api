49ba60fabe0bce97c9d7f6d284de47e0bbf2dd01
otsang3 <ohhtsang@gmail.com>
2020-07-16 18:48:04 +0100

    add files

---
+++ src/components/CharacterList.js
@@ -0,0 +1,51 @@
+import React, { useReducer, useEffect } from 'react';
+import CharacterListItem from './CharacterListItem.js';
+
+export const CharacterContext = React.createContext()
+
+const initialState = {
+    loading: true,
+    error: '',
+    characters: []
+}
+
+const reducer = (currentState, action) => {
+    switch(action.type) {
+        case 'FETCH_SUCCESS': 
+        return {
+            loading: false,
+            error: '',
+            characters: action.payload
+        };
+        case 'FETCH_ERROR':
+            return {
+                loading: false,
+                error: 'something went wrong',
+                characters: []
+            }
+    }
+}
+
+function CharacterList() {
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        fetch("https://rickandmortyapi.com/api/character/")
+        .then(res => res.json())
+        .then(data => {
+            dispatch({type: 'FETCH_SUCCESS', payload: data.results})
+        })
+        .catch(error => dispatch({type: 'FETCH_ERROR'}))
+    })
+
+    return(
+        <div>
+            <CharacterContext.Provider value={state}>
+                <CharacterListItem/>
+            </CharacterContext.Provider>
+        </div>
+    )
+}
+
+export default CharacterList;
---
+++ src/components/CharacterListItem.js
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react';
+import {CharacterContext} from './CharacterList.js';
+
+
+
+function CharacterListItem() {
+
+    const characterContext = useContext(CharacterContext);
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default CharacterListItem;
